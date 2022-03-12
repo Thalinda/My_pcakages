3c798c44b9cbdf86485aabc65b7b7375cd3581d2
Thalinda <thalinda.bandara@qualitapss.com>
2022-03-12 18:37:01 +0530

    Getting Data from Perviouse Quetions

--- packages/dynamicform/lib/commonfunction.dart
+++ packages/dynamicform/lib/commonfunction.dart
@@ -1,13 +1,15 @@
+// ignore_for_file: prefer_final_fields, unused_field
+
 part of dynamicform;
 
 class CommonFunction {
   static List<Map<String, dynamic>> _values = [];
-  DataHolders _dataHolders = Get.put(DataHolders());
+  // DataHolders _dataHolders = Get.put(DataHolders());
   static List dataset = [];
   void _onUpdate(key, val, isrequired, label) {
-    for (int i = 0; i < _dataHolders.dataHolder.length; i++) {
-      if (_dataHolders.dataHolder[i]['id'] == key) {
-        _dataHolders.dataHolder.removeAt(i);
+    for (int i = 0; i < dataset.length; i++) {
+      if (dataset[i]['id'] == key) {
+        dataset.removeAt(i);
         break;
       }
     }
@@ -19,30 +21,25 @@ class CommonFunction {
       'label': label
     };
 
-    _dataHolders.dataHolder.add(json);
+    dataset.add(json);
   }
 
   getformdata() {
     List<Map<String, dynamic>> data1 = [];
     List<Map<String, dynamic>> errors = [];
 
-    for (var i = 0; i < _dataHolders.dataHolder.length; i++) {
-      if (_dataHolders.dataHolder[i]['isrequired'] == 1) {
-        if (_dataHolders.dataHolder[i]['value'] == null ||
-            _dataHolders.dataHolder[i]['value'] == '' ||
-            _dataHolders.dataHolder[i]['value'].isEmpty) {
-          errors.add({'error': _dataHolders.dataHolder[i]['label']});
+    for (var i = 0; i < dataset.length; i++) {
+      if (dataset[i]['isrequired'] == 1) {
+        if (dataset[i]['value'] == null ||
+            dataset[i]['value'] == '' ||
+            dataset[i]['value'].isEmpty) {
+          errors.add({'error': dataset[i]['label']});
         } else {
-          data1.add({
-            "quizId": _dataHolders.dataHolder[i]['id'],
-            "answer": _dataHolders.dataHolder[i]['value']
-          });
+          data1
+              .add({"quizId": dataset[i]['id'], "answer": dataset[i]['value']});
         }
       } else {
-        data1.add({
-          "quizId": _dataHolders.dataHolder[i]['id'],
-          "answer": _dataHolders.dataHolder[i]['value']
-        });
+        data1.add({"quizId": dataset[i]['id'], "answer": dataset[i]['value']});
       }
     }
 
